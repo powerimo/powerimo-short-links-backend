a2cb8e554273bf418fb61f1e0a463e403e7563d6
andewil <voronin@andewil.com>
2024-12-03 05:54:21 +0100

    qa (#10)
    * tests
    
    * tests

--- cicd/bash/deploy-server.sh
+++ cicd/bash/deploy-server.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ENVIRONMENT=$1
+CONTAINER_NAME=$1
+IMAGE_NAME=$2
+APP_ENV=$3
 
-if [ "$ENVIRONMENT" == "QA" ]; then
-    echo "Deploying to QA environment"
-    # QA deployment steps
-elif [ "$ENVIRONMENT" == "PROD" ]; then
-    echo "Deploying to PROD environment"
-    # PROD deployment steps
-    DC_NAME="powerimo-sl-server-prod"
+# log
+echo "Starting container ${CONTAINER_NAME} based on the image ${IMAGE_NAME}"
+echo "env: ${APP_ENV}"
 
-    # shellcheck disable=SC1090
-    source ~/config/powerimo-sl-vars-prod
+# read local vars
+echo "Sourcing vars..."
+# shellcheck disable=SC1090
+source ~/config/"${CONTAINER_NAME}"/vars
 
-    docker stop ${DC_NAME} > /dev/null
-    docker container rm ${DC_NAME} > /dev/null
-    # shellcheck disable=SC2153
-    docker pull "${DI_NAME}"
+echo "Stop previous launched container"
+docker stop "${CONTAINER_NAME}" > /dev/null
+docker rm "${CONTAINER_NAME}" > /dev/null
+docker pull "${IMAGE_NAME}"
 
-    docker run -d --restart unless-stopped \
-        --name=${DC_NAME} \
-        -e SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
-        -e SPRING_DATASOURCE_USER="${SPRING_DATASOURCE_USER}" \
-        -e SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
-        -e APP_ENV="prod" \
-        -p 15500:8080 \
-        "${DI_NAME}"
-else
-    echo "Unknown environment: $ENVIRONMENT"
-    exit 1
-fi
+echo "Starting the container..."
+docker run -d --restart unless-stopped \
+    --name="${CONTAINER_NAME}" \
+    -e SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
+    -e SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
+    -e SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
+    -e APP_ENV="${APP_ENV}" \
+    -e APP_BASE_URL="${APP_BASE_URL}" \
+    -p 15520:8080 \
+    "${IMAGE_NAME}"
+
+echo "Container has been started"
